c7d7f46d4cc8b4d7e8201f1fdbaf911cdf5acb1c
Vinicius Boschi <vinicius.yg@gmail.com>
2022-02-15 17:46:06 -0300

    Adicionando os projetos e o readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Portfolio
+### **Portfolio**
+
+### Um [site](https://portfolioviniciusboschi.netlify.app/) que foi feito para conter todas as minhas informações.
+
+##
+
+1. Objetivos do site<br><br>
+O objetivo deste site é conter todas as informações sobre mim, como, redes socias, projetos, sobre mim.
+
+2. Seção Inicial<br><br>
+<img src="images/home.jpg" alt="Seção 1"> 
+
+3. Tecnologia usadas: <br>
+
+* <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="javascript"><br>
+* <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="html"><br>
+* <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="css"><br>
+* <img src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white">
---
+++ js/projects.js
@@ -0,0 +1,62 @@
+let projects = [
+
+    {
+      link: "https://gatitopetshop.netlify.app/",
+      image: "images/gatito-petshop.jpg",
+      text: "gatito petshop"
+    },
+  
+    {
+      link: "https://gamestores.netlify.app/",
+      image: "images/gamestore.jpg",
+      text: "loja de jogos"
+    },
+  
+    {
+      link: "https://barbeariasalura.netlify.app/",
+      image: "images/barbearia.jpg",
+      text: "barbearia"
+    },
+  
+    {
+      link: "https://musicsflix.netlify.app/",
+      image: "images/musicsflix.jpg",
+      text: "musicsflix"
+    },
+  
+    {
+      link: "https://fotografias.netlify.app/",
+      image: "images/fotografia.jpg",
+      text: "boschi fotos"
+    },
+  
+    {
+      link: "https://visitcards.netlify.app/",
+      image: "images/nlw-heat.jpg",
+      text: "cartão"
+    },
+  
+    {
+      link: "https://boschiflix.netlify.app/",
+      image: "images/boschiflix.jpg",
+      text: "boschiflix"
+    },
+  
+    {
+      link: "https://formup.netlify.app/",
+      image: "images/formulario.jpg",
+      text: "formulário"
+    },
+  
+    {
+      link: "https://hzcskate.netlify.app/",
+      image: "images/hzcskate.jpg",
+      text: "hzc skate"
+    },
+  
+    {
+      link: "https://challengesballerini.netlify.app/",
+      image: "images/desafios-ballerini.jpg",
+      text: "desafios"
+    },
+  ]
