d8d00e4df5cd8ac368fc0e1ef2286de396dc3216
Vinicius Boschi <vinicius.yg@gmail.com>
2022-04-12 14:53:35 -0300

    Arrumando fotos e informações de contato

--- js/projects.js
+++ js/projects.js
@@ -2,61 +2,61 @@ let projects = [
 
     {
       link: "https://gatitopetshop.netlify.app/",
-      image: "images/gatito-petshop.jpg",
+      image: "images/projects/gatito-petshop.jpg",
       text: "gatito petshop"
     },
   
     {
       link: "https://gamestores.netlify.app/",
-      image: "images/gamestore.jpg",
+      image: "images/projects/gamestore.jpg",
       text: "loja de jogos"
     },
   
     {
       link: "https://barbeariasalura.netlify.app/",
-      image: "images/barbearia.jpg",
+      image: "images/projects/barbearia.jpg",
       text: "barbearia"
     },
   
     {
       link: "https://musicsflix.netlify.app/",
-      image: "images/musicsflix.jpg",
+      image: "images/projects/musicsflix.jpg",
       text: "musicsflix"
     },
   
     {
       link: "https://fotografias.netlify.app/",
-      image: "images/fotografia.jpg",
+      image: "images/projects/fotografia.jpg",
       text: "boschi fotos"
     },
   
     {
       link: "https://visitcards.netlify.app/",
-      image: "images/nlw-heat.jpg",
+      image: "images/projects/nlw-heat.jpg",
       text: "cartão"
     },
   
     {
       link: "https://boschiflix.netlify.app/",
-      image: "images/boschiflix.jpg",
+      image: "images/projects/boschiflix.jpg",
       text: "boschiflix"
     },
   
     {
       link: "https://formup.netlify.app/",
-      image: "images/formulario.jpg",
+      image: "images/projects/formulario.jpg",
       text: "formulário"
     },
   
     {
       link: "https://hzcskate.netlify.app/",
-      image: "images/hzcskate.jpg",
+      image: "images/projects/hzcskate.jpg",
       text: "hzc skate"
     },
   
     {
       link: "https://challengesballerini.netlify.app/",
-      image: "images/desafios-ballerini.jpg",
+      image: "images/projects/desafios-ballerini.jpg",
       text: "desafios"
     },
   ]
