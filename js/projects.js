8a078e9c4c0ec93bd7a8a2f5eb0db3d456226464
Vinicius Boschi <vinicius.yg@gmail.com>
2022-06-29 19:40:06 -0300

    Mudando os projetos

--- js/projects.js
+++ js/projects.js
@@ -1,35 +1,4 @@
 let projects = [
-  
-  {
-    link: "https://gatitopetshop.netlify.app/",
-    image: "images/projects/gatito-petshop.jpg",
-    text: "gatito petshop"
-  },
-
-  {
-    link: "https://gamestores.netlify.app/",
-    image: "images/projects/gamestore.jpg",
-    text: "loja de jogos"
-  },
-
-  {
-    link: "https://barbeariasalura.netlify.app/",
-    image: "images/projects/barbearia.jpg",
-    text: "barbearia"
-  },
-
-  {
-    link: "https://musicsflix.netlify.app/",
-    image: "images/projects/musicsflix.jpg",
-    text: "musicsflix"
-  },
-
-  {
-    link: "https://fotografias.netlify.app/",
-    image: "images/projects/fotografia.jpg",
-    text: "boschi fotos"
-  },
-
   {
     link: "https://buscarceps.netlify.app/",
     image: "images/projects/cep.jpg",
@@ -37,38 +6,32 @@ let projects = [
   },
 
   {
-    link: "https://boschiflix.netlify.app/",
-    image: "images/projects/boschiflix.jpg",
-    text: "boschiflix"
-  },
-
-  {
-    link: "https://formup.netlify.app/",
-    image: "images/projects/formulario.jpg",
-    text: "formulário"
-  },
-
-  {
-    link: "https://hzcskate.netlify.app/",
-    image: "images/projects/hzcskate.jpg",
-    text: "hzc skate"
-  },
-
-  {
-    link: "https://vanillaprojects.netlify.app/",
-    image: "images/projects/projetos-js.jpg",
-    text: "projetos js"
-  },
-
-  {
-    link: "https://optimustech.netlify.app/",
-    image: "images/projects/optimusTech.jpg",
-    text: "optimus tech"
-  },
-
-  {
     link: "https://github.com/Vinicius-Boschi/Filmes",
     image: "images/projects/movies.jpg",
     text: "filmes"
-  }
+  },
+
+  {
+    link: "https://playerofmusics.netlify.app/",
+    image: "images/projects/musicplayer.jpg",
+    text: "player de música"
+  },
+
+  {
+    link: "https://weathercitys.netlify.app/",
+    image: "images/projects/climatempo.jpg",
+    text: "climatempo"
+  },
+
+  {
+    link: "https://descubrasuageracao.netlify.app/",
+    image: "images/projects/generation.jpg",
+    text: "descubra sua geração"
+  },
+
+  {
+    link: "https://doctorscare.netlify.app/",
+    image: "images/projects/doctorcare.jpg",
+    text: "doctor care"
+  },
 ]
