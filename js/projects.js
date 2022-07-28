ace1acc50b4f9cb62ab6c3b67975c7d8d13c2f20
Vinicius Boschi <vinicius.yg@gmail.com>
2022-07-28 14:44:53 -0300

    Removendo pasta de fotos dos projetos

--- js/projects.js
+++ js/projects.js
@@ -1,53 +1,53 @@
 export let projects = [
   {
     link: "https://buscarceps.netlify.app/",
-    image: "images/projects/cep.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181602722-bbf67a05-9ea3-4c19-9a85-1d0f237515c3.jpg",
     text: "buscar cep"
   },
 
   {
     link: "https://github.com/Vinicius-Boschi/Filmes",
-    image: "images/projects/movies.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181602651-bc107309-7646-4a5b-a7ba-916a526bb818.jpg",
     text: "filmes"
   },
 
   {
     link: "https://playerofmusics.netlify.app/",
-    image: "images/projects/musicplayer.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181602614-afb8a293-156b-45d9-95e4-3c077f66f317.jpg",
     text: "player de música"
   },
 
   {
     link: "https://weathercitys.netlify.app/",
-    image: "images/projects/climatempo.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181602562-972e9d64-ee52-4098-a047-a92a3c88d5fd.jpg",
     text: "climatempo"
   },
 
   {
     link: "https://descubrasuageracao.netlify.app/",
-    image: "images/projects/generation.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181602517-25d0f673-9c54-403d-93dd-6e49fb7c7ad0.jpg",
     text: "descubra sua geração"
   },
 
   {
     link: "https://doctorscare.netlify.app/",
-    image: "images/projects/doctorcare.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181602236-fbcd5e60-1b25-444e-b2e2-690f6c8ba2ed.jpg",
     text: "doctor care"
   },
   
   {
     link: "https://pokedexapipokemon.netlify.app/",
-    image: "images/projects/pokedex.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/177438025-ccd8a8f7-f445-4214-9a91-6251f848e799.jpg",
     text: "pokedex"
   },
   {
     link: "https://adopetalura.netlify.app/",
-    image: "images/projects/adopet.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/178602779-ecd6cf5f-0562-47bb-9316-3795ff660099.jpg",
     text: "AdoPet Alura"
   },
   {
     link: "https://dashboardstarwars.netlify.app/",
-    image: "images/projects/star-wars.jpg",
+    image: "https://user-images.githubusercontent.com/74377158/181092726-cf0b5406-1326-4b00-8212-66be228aff16.jpg",
     text: "Star Wars"
   }
 ]
