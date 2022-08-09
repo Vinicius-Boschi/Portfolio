d1b95ae2d4921913c1d1a7a2296371028c4823bf
Vinicius Boschi <vinicius.yg@gmail.com>
2022-08-09 18:06:03 -0300

    Refazendo a seção de projetos

--- js/projects.js
+++ js/projects.js
@@ -2,52 +2,63 @@ export let projects = [
   {
     link: "https://buscarceps.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/181602722-bbf67a05-9ea3-4c19-9a85-1d0f237515c3.jpg",
-    text: "buscar cep"
+    name: "buscar cep",
+    text: "Projeto de buscar cep usando a api do viaCep. Para fazer a requisição da api utilizei a função fetch()."
   },
 
   {
     link: "https://github.com/Vinicius-Boschi/Filmes",
-    image: "https://user-images.githubusercontent.com/74377158/181602651-bc107309-7646-4a5b-a7ba-916a526bb818.jpg",
-    text: "filmes"
+		image: "https://user-images.githubusercontent.com/74377158/181602651-bc107309-7646-4a5b-a7ba-916a526bb818.jpg",
+    name: "filmes",
+    text: "Projeto que mostra os filmes mais recentes lançados, também podendo favoritar/desfavoritar os filmes de acordo com o seu gosto. Utiliza-se o async fetch() para a requisição da api do The Movie Database."
   },
 
   {
     link: "https://playerofmusics.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/181602614-afb8a293-156b-45d9-95e4-3c077f66f317.jpg",
-    text: "player de música"
+    name: "player de música",
+    text: "Projeto feito para similar ao Spotify, podendo pular, voltar, dar play e pause nas músicas."
   },
 
   {
     link: "https://weathercitys.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/181602562-972e9d64-ee52-4098-a047-a92a3c88d5fd.jpg",
-    text: "climatempo"
+    name: "climatempo",
+    text: "Projeto de clima que mostra o clima da cidade de São Paulo por padrão, mas também pode mostrar o das outras cidades apenas pesquisando por elas no campo de pesquisa."
   },
 
   {
     link: "https://descubrasuageracao.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/181602517-25d0f673-9c54-403d-93dd-6e49fb7c7ad0.jpg",
-    text: "descubra sua geração"
+    name: "descubra sua geração",
+    text: "Projeto que mostra sua geração de acordo com sua idade, podendo ser ela Baby Boomer, X, Millennials, Z e Alpha."
   },
 
   {
     link: "https://doctorscare.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/181602236-fbcd5e60-1b25-444e-b2e2-690f6c8ba2ed.jpg",
-    text: "doctor care"
+    name: "doctor care",
+    text: "Projeto da RocketSeat feito para treinar programação. Um site de assistência médica, podendo marcar sua consulta."
   },
   
   {
     link: "https://pokedexapipokemon.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/177438025-ccd8a8f7-f445-4214-9a91-6251f848e799.jpg",
-    text: "pokedex"
+    name: "pokedex",
+    text: "Projeto de Pokedex para conter várias informações dos pokémons e também treinar requisição de api usando o fetch()."
   },
+  
   {
     link: "https://adopetalura.netlify.app/",
-    image: "https://user-images.githubusercontent.com/74377158/178602779-ecd6cf5f-0562-47bb-9316-3795ff660099.jpg",
-    text: "AdoPet Alura"
+		image: "https://user-images.githubusercontent.com/74377158/178602779-ecd6cf5f-0562-47bb-9316-3795ff660099.jpg",
+    name: "adoPet alura",
+    text: "Site que contém alguns animais que podem ser adotados. E podendo treinar construção de formulários."
   },
+  
   {
     link: "https://dashboardstarwars.netlify.app/",
     image: "https://user-images.githubusercontent.com/74377158/181092726-cf0b5406-1326-4b00-8212-66be228aff16.jpg",
-    text: "Star Wars"
+    name: "star wars",
+    text: "Projeto feito para conter todas as informações do mundo do Star Wars e também aprendendo a usar o axios()."
   }
 ]
--- js/script.js
+++ js/script.js
@@ -2,47 +2,34 @@ import { projects } from './projects.js'
 
 // Data.
 const date = document.getElementById("date")
-date.innerHTML = new Date().getFullYear() // Mostra o ano atual.
+date.innerHTML = new Date().getFullYear() 
 
 // Projetos.
-let projectsPortfolio = document.querySelector(".portfolio__projects") // Aqui que vai ser colocado os itens dessa função abaixo.
+let projectsPortfolio = document.querySelector(".portfolio__infos")
 
 function showProjects() {
-    for (let i = 0; i < projects.length; i++) { // Aqui ele pega todas as informações do array criado e mostra na tela.
-      projectsPortfolio.innerHTML += "<div class='portfolio__projects'><a class='portfolio__link' href='" + projects[i].link +"'target=_blank''>" +  "<img class='portfolio__img' src='" + projects[i].image + "'>" + "<p class='portfolio__text'>" + projects[i].text + "</p>" + "</div>"
-    }
+  for (let i = 0; i < projects.length; i++) { 
+    projectsPortfolio.innerHTML += "<div class='portfolio__content'>" + "<img class='portfolio__img' src='" + projects[i].image + "'>" + "<div class='portfolio__container'>" + "<h2 class='portfolio__h2'>" + projects[i].name + "</h2>" + "<p class='portfolio__text'>" + projects[i].text + "<input type='submit' value='Ir para o projeto' class='portfolio__input' onclick=location.href='" + projects[i].link + "'>" + "</p>" + "</div>" + "</div>"
+  }
 }
 
-showProjects() // Aqui ele vai mostrar a função na tela.
-
-function load() {
-  let count = 7 // Setamos o valor inicial.
-  $(".portfolio__projects").slice(count).hide() // Escondemos todos os elementos maior que o valor inicial.
-
-  $('.portfolio__load').click(function() {
-    count += 3 // Somamos a quantidade nova a ser exibida.
-      for (let i = 0; i < count; i++) { // Rodamos o loop no valor total.
-        $(".portfolio__projects").eq(i).show(800) // Mostras os projetos com um delay.
-      }
-  }) 
-}
-load() // Aqui ele vai mostrar a função na tela.
+showProjects()
 
 // Animação de escrita.
 
-function typeWrite(element){ // Função que deixa o texto com uma animação de escrita.
+function typeWrite(element){ 
   const textoArray = element.innerHTML.split('')
   element.innerHTML = ' '
   textoArray.forEach(function(letter, i){   
 
-    setTimeout(function(){ // Definindo o tempo de duração da animação.
-        element.innerHTML += letter
+    setTimeout(function(){ 
+      element.innerHTML += letter
     }, 500 * i)
   })
 }
 
-const tittle = document.querySelector('.header__effect') // Onde vai ser aplicada a função.
-typeWrite(tittle) // Mostrará na tela.
+const tittle = document.querySelector('.header__effect') 
+typeWrite(tittle) 
 
 // Navbar Responsiva.
 
@@ -52,13 +39,13 @@ let links = document.querySelector(".header__list")
 let burguer = document.querySelector(".header__burger")
 
 button.addEventListener("click", function() {
-  const containerHeight = linksContainer.getBoundingClientRect().height // Mostra o height atual dos links.
+  const containerHeight = linksContainer.getBoundingClientRect().height 
   const linksHeight = links.getBoundingClientRect().height
 
   if (containerHeight === 0) {
-    linksContainer.style.height = `${linksHeight}px` // Se for estritamente igual a 0 ele adiciona o height no navbar, fazendo com que ele mostre os links nele.
+    linksContainer.style.height = `${linksHeight}px` 
   } else {
-    linksContainer.style.height = 0 // Se não ele recolhe tudo.
+    linksContainer.style.height = 0 
   }
 }) 
 
@@ -76,7 +63,7 @@ let navbar = document.querySelector(".header__navbar")
 let btn = document.querySelector(".btn")
 
 window.addEventListener("scroll", function() {
-  const scrollHeight = window.pageYOffset // Quando descer a página a navbar fica fixa.
+  const scrollHeight = window.pageYOffset 
   const navHeight = navbar.getBoundingClientRect().height
 
   if (scrollHeight > navHeight) {
@@ -85,19 +72,19 @@ window.addEventListener("scroll", function() {
     navbar.classList.remove("header__navbar-fixed")
   }
 
-  if (scrollHeight > 500) { // Se for maior que 500 o botão aparece.
+  if (scrollHeight > 500) { 
     btn.classList.add('show')
-  } else { // Se for menor ele desaparece.
+  } else { 
     btn.classList.remove('show')
   }
 })
 
 // Animação de conteúdo vindo pela esquerda.
-AOS.init() // Chama a função AOS para deixar os item escolhidos com animação. Ela vem dos link que são importados no html da página principal.
+AOS.init()
 
-// // Animação de carregamento.
+// Animação de carregamento.
 const preloader = document.querySelector(".preloader")
 
 window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader") // Toda vez que a página for recarregada, essa função irá funcionar.
+  preloader.classList.add("hide-preloader")
 })
